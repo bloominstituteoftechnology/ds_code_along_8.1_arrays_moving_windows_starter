9061add7fc699dce47d2040a8c967f3a73bd43ee
Ladrillo <gabriel@cabrejas.net>
2022-07-15 19:50:55 +0200

    initial

---
+++ 1_2D_Arrays.py
@@ -0,0 +1,18 @@
+def solution(size):
+    # your code here
+    return "hello world"
+
+
+# Create a square 2D array (list type in Python) of size size x size. Fill the array with 0's.
+# Then draw a line of 1's down the main diagonal.
+# This is called an identity matrix.
+
+# Example
+# Input: size = 4
+# Output: solution(4) =
+# [
+#     [1, 0, 0, 0],
+#     [0, 1, 0, 0],
+#     [0, 0, 1, 0],
+#     [0, 0, 0, 1]
+# ]
---
+++ README.md
@@ -0,0 +1,14 @@
+# Code-Along 8.1 - Arrays and Moving Windows
+
+## Requirements
+
+- Python 3 installed locally.
+
+## Set Up
+
+1. Clone this repository.
+2. Execute the scripts using Python. Example: `python3 1_2D_Arrays.py`.
+
+**NOTE:**
+
+These challenges can be loaded in [Code Signal](https://codesignal.com/), if you'd prefer to practice using their platform instead of local tools.
